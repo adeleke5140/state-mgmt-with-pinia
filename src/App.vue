6d124c5d74a7fe500e0e6be039e9002386822f07
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-09-03 11:15:35 +0100

    experimenting with Pinia

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { ref } from "vue"
+import { useUserStore } from "./stores/users"
+
+const user_store = useUserStore()
+
+const user_input = ref({
+  name: "",
+  email: ""
+})
+
+const sort = ref(false)
+
+const CreateUser = () => {
+  if (!user_input.value.name || !user_input.value.email)
+    return alert("Please enter both name and email")
+
+  user_store.create(user_input.value)
+
+  user_input.value = {
+    name: "",
+    email: ""
+  }
+}
+</script>
+
+<template>
+  <div>
+    <main>
+      <h1>Team manager</h1>
+      <form @submit.prevent="CreateUser">
+        <label for="name">
+          Name:
+          <input
+            type="text"
+            placeholder="e.g Naruto Uzumaki"
+            v-model="user_input.name"
+            id="name"
+          /> </label
+        ><br />
+        <label for="email">
+          Email:
+          <input
+            type="email"
+            placeholder="e.g. hokage@ninja.com"
+            v-model="user_input.email"
+            id="email"
+          />
+        </label>
+        <br />
+        <input type="submit" value="Create User" />
+      </form>
+
+      <label for="sort">
+        <span>Sort</span>
+        <input type="checkbox" v-model="sort" id="sort" />
+      </label>
+
+      <div class="users" v-if="!sort">
+        <div class="user" v-for="user in user_store.users">
+          {{ user.name }}
+        </div>
+      </div>
+
+      <div class="users" v-else>
+        <div class="user" v-for="user in user_store.usersByName">
+          {{ user.name }}
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+
+table {
+  margin-top: 40px;
+}
+</style>
